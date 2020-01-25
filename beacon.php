2d8b0cfdea950aaf9823c1f1cf8ceef2d2f53fcd
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-01-25 10:01:23 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -66,7 +66,7 @@ $jsonFlex = [
             "type" => "text",
             "text" => "$new_date",
             "align" => "center",                   
-            "size" => "2xl",
+            "size" => "3xl",
             "weight" => "bold",
             "color" => "#000000"
           ],
