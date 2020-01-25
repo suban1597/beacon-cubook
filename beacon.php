e95ca0e42829548a9de2860a6417e8b524203fc6
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-01-25 09:59:38 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -116,7 +116,7 @@ $jsonFlex = [
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
         error_log(json_encode($event));
-//        $reply_message = '';
+        $reply_message = '';
         $reply_token = $event['replyToken'];
 
         $data = [
