8816a5d02ecfae6764a8e063133ac5ea2ea1dd08
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-14 16:14:33 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -37,14 +37,24 @@ echo $new_date;
 $new_time = date("H:i:s");
 echo $new_time;
 
-        
 $objConnect = mssql_connect("203.154.162.41","sa","Adminchul@book1") or die("Error Connect to Database");
 $objDB = mssql_select_db("Line_Project");
 $strSQL = "INSERT INTO Attend ";
 $strSQL .="(BTIME,BDATE) ";
 $strSQL .="VALUES ";
-$strSQL .="('$new_time','$new_date')";
+$strSQL .="('A','B')";
+
+$objQuery = mssql_query($strSQL);
+if($objQuery)
+{
+	echo "Save Done.";
+}
+else
+{
+	echo "Error Save [".$strSQL."]";
+}
 mssql_close($objConnect);
+
 
 $jsonFlex = [
     "type" => "flex",
