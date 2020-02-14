2f5bca421fbcefdd06eb34a014b14c5941d98c69
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-14 18:41:00 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -145,8 +145,8 @@ function send_reply_message($url, $post_header, $post_body)
     curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_URL, 'https://secure.chulabook.com/Line/insert_db.php');
     $result = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL, 'https://secure.chulabook.com/Line/insert_db.php');
     curl_close($ch);
 
     return $result;
