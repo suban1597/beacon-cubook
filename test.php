2978ec21064a38b66bc72087f201d4fe83c238e8
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 12:56:36 +0700

    Update test.php

--- test.php
+++ test.php
@@ -15,7 +15,7 @@
 	$encodeJson = json_encode($messages);
 
 	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
-  	$LINEDatas['token'] = "4rdPrwctgzH4f52jYbtvykZkki+MN2xZK+finbLthiCRMR1fyBLt+dlKU8ExfQxxcV7ZC9VX+lG/VG5XnZHp7xuoVQs4tyoCNUA0TxZek8M1DqArJBPPalW51qk2NWCu0L1En5v+FdTDw3csua882gdB04t89/1O/w1cDnyilFU=";
+  	$LINEDatas['token'] = "A6xkM4k1GiZ9lLeIJPDXXsjbmJDn9N7zts3hK71ygtu";
 
   	$results = sentMessage($encodeJson,$LINEDatas);
 
