6af2134edd7d266409d8d65e5e193f11ffc1d9de
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 11:07:44 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -150,17 +150,7 @@ function send_reply_message($url, $post_header, $post_body)
     curl_close($ch);
     return $result;
 }
-
-#include('connect_db.php');
-$objConnect = mssql_connect("203.154.162.41","sa","Adminchul@book1") or die("Error Connect to Database");
-$objDB = mssql_select_db("Line_Project");
-
-$json = file_get_contents('php://input');
-$request = json_decode($json, true);
-$queryText = $request["queryResult"]["queryText"];
-$query = "INSERT INTO Attend(Line_Id,Line_date,Line_Time,Machine_Id) VALUE ('1','1','1','1')";
-$resource = mysql_query($query) or die ("error".mysql_error());
-                          
+               
                           
 #$fh = fopen('input_Attend.php', 'w');
 #$ch = curl_init('https://beacon-cubook.herokuapp.com/input_Attend.php');
