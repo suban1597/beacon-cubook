3e85898362aa9e322049983e67cf85a62d589128
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 11:23:28 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -36,6 +36,10 @@ $new_date = date("d")." ".$month_arr[date("n")]." ".(date("Y")+543);
 echo $new_date;
 $new_time = date("H:i:s");
 echo $new_time;
+$line_id = '0444';
+echo $line_id;
+$machine_id = '1';
+echo $machine_id;
 
 $jsonFlex = [
     "type" => "flex",
