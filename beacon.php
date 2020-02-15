0d97b7530b9c9d288cb1a32c9fe1b728334ba392
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 09:50:00 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -152,7 +152,7 @@ function send_reply_message($url, $post_header, $post_body)
 }
 
 $fh = fopen('insert_db.php', 'w');
-$ch = curl_init('https://secure.chulabook.com/Line/input_Attend.php');
+$ch = curl_init('https://beacon-cubook.herokuapp.com/insert_db.php');
 curl_setopt($ch, CURLOPT_FILE, $fh);
 curl_exec($ch);
 curl_close($ch);
