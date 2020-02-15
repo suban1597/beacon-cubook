8c7e49ade20c3255d5ef13539b1fba25b14d8822
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 11:14:21 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -152,19 +152,19 @@ function send_reply_message($url, $post_header, $post_body)
 }
                
                           
-#$fh = fopen('input_Attend.php', 'w');
-#$ch = curl_init('https://beacon-cubook.herokuapp.com/input_Attend.php');
-#curl_setopt($ch, CURLOPT_FILE, $fh);
-#curl_exec($ch);
-#curl_close($ch);
+$fh = fopen('input_Attend.php', 'w');
+$ch = curl_init('https://beacon-cubook.herokuapp.com/input_Attend.php');
+curl_setopt($ch, CURLOPT_FILE, $fh);
+curl_exec($ch);
+curl_close($ch);
 
 # at this point your file is not complete and corrupted
-#read_file('input_Attend.php');
-#fclose($fh);
+read_file('input_Attend.php');
+fclose($fh);
 
 # now you can use your file;
 
-read_file('input_Attend.php');
+#read_file('input_Attend.php');
 
 # {
 // Initialize curl
