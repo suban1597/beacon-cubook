17a5897e5c1a7181f76762358ca7a0eebb697baa
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 10:59:53 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -151,11 +151,13 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 
-include('connect_db.php');
+#include('connect_db.php');
+$objConnect = mssql_connect("203.154.162.41","sa","Adminchul@book1") or die("Error Connect to Database");
+$objDB = mssql_select_db("Line_Project");
+
 $json = file_get_contents('php://input');
 $request = json_decode($json, true);
 $queryText = $request["queryResult"]["queryText"];
-#$userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
 $query = "INSERT INTO Attend(Line_Id,Line_date,Line_Time,Machine_Id) VALUE ('0444','$new_date','$new_time','1')";
 $resource = mysql_query($query) or die ("error".mysql_error());
                           
