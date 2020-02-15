c60ff80e10e743ba21a4babd979b3688befe0c25
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 11:05:32 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -158,7 +158,7 @@ $objDB = mssql_select_db("Line_Project");
 $json = file_get_contents('php://input');
 $request = json_decode($json, true);
 $queryText = $request["queryResult"]["queryText"];
-$query = "INSERT INTO Attend(Line_Id,Line_date,Line_Time,Machine_Id) VALUE ("0444","1","1","1")";
+$query = "INSERT INTO Attend(Line_Id,Line_date,Line_Time,Machine_Id) VALUE ('1','1','1','1')";
 $resource = mysql_query($query) or die ("error".mysql_error());
                           
                           
