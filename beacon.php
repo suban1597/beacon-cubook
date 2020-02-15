cc533f29254945f6a284d1f6340c09b3fa2a8b9d
suban1597 <59216475+suban1597@users.noreply.github.com>
2020-02-15 12:37:32 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -45,7 +45,7 @@ $json = file_get_contents('php://input');
 $request = json_decode($json, true);
 $queryText = $request["queryResult"]["queryText"];
 $line_Id = $request['originalDetectIntentRequest']['payload']['data']['source']['line_id'];
-$query = "INSERT INTO Attend(Line_Id,Line_Date,Line_Time,Machine_Id) VALUE ('$line_id','$new_date','$new_time')";
+$query = "INSERT INTO Attend(Line_Id,Line_Date,Line_Time,Machine_Id) VALUE ('$line_id','$new_date','$new_time','$machine_id')";
 $resource = mysql_query($query) or die ("error".mysql_error());
 echo $line_id;
 
